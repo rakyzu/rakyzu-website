29db6cbb864c29fc9294f5ed5542058be14e592f
rakyzu <rakyzu@users.noreply.github.com>
2026-07-29 13:56:35 +0000

    feat: add layout, SEO, sitemap, index page, animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://rakyzu.my.id",
   output: "server",
   adapter: cloudflare(),
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
